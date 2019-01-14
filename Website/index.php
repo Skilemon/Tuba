1f82482ab77a3eb8647cdf47752b820af17572fe
Lemon <memorysmail@foxmail.com>
2019-01-14 14:19:51 +0800

    Update

--- Website/index.php
+++ Website/index.php
@@ -2,9 +2,14 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Test</title>
+    <title>Tuba API</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="layui/css/layui.css">
+    <script src="layui/layui.js"></script>
+    <script src="js/jquery-3.3.1.js"></script>
+    <script src="js/popper.js"></script>
 </head>
 <body>
     <?php
