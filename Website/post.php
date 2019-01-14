f1869641f52124972f343a860fe5a2fbdacceec1
Lemon <memorysmail@foxmail.com>
2019-01-15 01:45:00 +0800

    Update

---
+++ Website/post.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Upload - Tuba API</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+<body>
+    <?php
+    $file = $imei = "";
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+    $file = input($_POST["file"]);
+    $imei = input($_POST["imei"]);
+    }
+    
+    function input($data)
+    {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+    }
+    ?>
+    
+</body>
+</html>
